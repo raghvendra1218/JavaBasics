464730ab4cf69d00533bbd474415460891617027
raghvendra1218 <raghvendra.dixit@sjsu.edu>
2019-01-20 00:56:34 +0530

    Add Bubble sort

--- BubbleSort/src/com/raghvendra/Bubble.java
+++ BubbleSort/src/com/raghvendra/Bubble.java
@@ -1,8 +1,11 @@
 package com.raghvendra;
 
+//Time complexity O(n^2)
+//Space complexity O(1)
+
 public class Bubble {
     public int[] bubbleSort(int[] arr){
-        for(int k = 0; k < arr.length; k++) {
+        for(int k = 0; k < arr.length-1; k++) {
             for(int i =0, j=1; j < arr.length; i++, j++){
                 int temp = arr[i];                          // temp = 5,
                 if(arr[i] > arr[j]){                        //
