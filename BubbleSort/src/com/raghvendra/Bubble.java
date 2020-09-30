df7c6c44b6cd8ea77cdd51e0734dda8f7a2dd6ea
Aankit Singh <52100296+annkit-singh@users.noreply.github.com>
2020-10-01 00:45:55 +0530

    Update Bubble.java
    Optimized bubble sort added

--- BubbleSort/src/com/raghvendra/Bubble.java
+++ BubbleSort/src/com/raghvendra/Bubble.java
@@ -4,6 +4,7 @@ package com.raghvendra;
 //Space complexity O(1)
 
 public class Bubble {
+    
     public int[] bubbleSort(int[] arr){
         for(int k = 0; k < arr.length-1; k++) {
             for(int i =0, j=1; j < arr.length-k; i++, j++){
@@ -16,4 +17,25 @@ public class Bubble {
         }
         return arr;
     }
+    public void optimizeBubbleSort(int array[]) {
+        int count = 0;  //counts the total comparisons 
+        for (int i = 0; i < array.length; i++) {
+            count = 0;
+
+            for (int j = 0; j < array.length - 1; j++) {
+            
+                if (array[j] > array[j + 1]) {
+                    count++;
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+                if (j == array.length - 2 && count == 0) 
+                    return; //if while iterating the whole array no comparison is made than array is sorted so return
+
+                     }
+
+             }
+    }
+    
 }
